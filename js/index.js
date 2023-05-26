65d1c5e0b500f7a9949b241f72e82658e66379a8
Nils Otto <nilsotto@gmx.de>
2023-05-26 14:30:03 +0200

    add JS to Answerbutton

--- js/index.js
+++ js/index.js
@@ -1,20 +1,38 @@
-const body = document.querySelector('[data-js="body"]');
+/*const body = document.querySelector('[data-js="body"]');
 const nav = document.querySelector('[data-js="nav"]');
 const header = document.querySelector('[data-js="header"]');
 const profileQuestions = document.querySelector('[data-js="profileQuestions"]');
 const profileBookmarks = document.querySelector('[data-js="profileBookmarks"]');
 const buttonDarkMode = document.querySelector('[data-js="buttonDarkMode"]');
 
-buttonDarkMode.addEventListener("click", () => {
+buttonDarkMode?.addEventListener("click", () => {
   body.classList.toggle("darkmode");
 });
-buttonDarkMode.addEventListener("click", () => {
+buttonDarkMode?.addEventListener("click", () => {
   nav.classList.toggle("darkmode1");
   header.classList.toggle("darkmode1");
   profileQuestions.classList.toggle("darkmode1");
   profileBookmarks.classList.toggle("darkmode1");
+  buttonDarkMode.textContent = "Lightmode";
+});*/
+
+const cardBookmark = document.querySelector('[data-js="cardBookmark"]');
+
+cardBookmark.addEventListener("click", () => {
+  cardBookmark.classList.toggle("bookmark--clicked");
 });
 
-buttonDarkMode.addEventListener("click", () => {
-  buttonDarkMode.textContent = "Lightmode";
-});
+const answerButton = document.querySelector('[data-js="answerButton"]');
+const cardAnswer = document.querySelector('[data-js="cardAnswer"]');
+
+answerButton.addEventListener("click", toggleAnswer);
+
+function toggleAnswer() {
+  cardAnswer.classList.toggle("hidden");
+
+  if (cardAnswer.classList.contains("hidden")) {
+    answerButton.textContent = "Show Answer";
+  } else {
+    answerButton.textContent = "Hide Answer";
+  }
+}
