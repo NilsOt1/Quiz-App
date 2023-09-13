3321ead8b828f84810c3dcf51451cef02933c967
Nils <nilsotto@gmx.de>
2023-09-13 15:32:33 +0200

    create new questions

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 const answerButtons = document.querySelectorAll('[data-js="answerButton"]');
 const cardAnswers = document.querySelectorAll('[data-js="cardAnswer"]');
 const questionForm = document.querySelector('[data-js="questionForm"]');
+const formMain = document.querySelector('[data-js="formMain"]');
+const formQuestion = document.querySelector('[data-js="question"]');
 
 answerButtons.forEach((button, answer) => {
   button.addEventListener("click", () => {
@@ -19,4 +21,23 @@ questionForm.addEventListener("submit", (event) => {
 
   const formData = new FormData(event.target);
   const data = Object.fromEntries(formData);
+
+  const newCard = document.createElement("section");
+  const newQuestion = document.createElement("p");
+  const newAnswer = document.createElement("p");
+  const newTags = document.createElement("p");
+
+  newCard.classList.add("card");
+  newQuestion.textContent = data.question;
+  newQuestion.classList.add("card__question");
+  newAnswer.classList.add("card__question");
+  newAnswer.textContent = data.answer;
+  newTags.classList.add("card__categories--flex");
+  newTags.textContent = data.tag;
+
+  formMain.append(newCard);
+  newCard.append(newQuestion, newAnswer, newTags);
+
+  event.target.reset();
+  formQuestion.focus();
 });
