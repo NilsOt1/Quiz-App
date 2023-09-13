e6fabeedc26ea7f34570a3d14f9f1ad9c66f7660
Nils <nilsotto@gmx.de>
2023-09-13 14:02:27 +0200

    show answer for each question

--- js/index.js
+++ js/index.js
@@ -1,36 +1,14 @@
-/*const body = document.querySelector('[data-js="body"]');
-const nav = document.querySelector('[data-js="nav"]');
-const header = document.querySelector('[data-js="header"]');
-const profileQuestions = document.querySelector('[data-js="profileQuestions"]');
-const profileBookmarks = document.querySelector('[data-js="profileBookmarks"]');
-const buttonDarkMode = document.querySelector('[data-js="buttonDarkMode"]');
-
-buttonDarkMode?.addEventListener("click", () => {
-  body.classList.toggle("darkmode");
-});
-buttonDarkMode?.addEventListener("click", () => {
-  nav.classList.toggle("darkmode1");
-  header.classList.toggle("darkmode1");
-  profileQuestions.classList.toggle("darkmode1");
-  profileBookmarks.classList.toggle("darkmode1");
-  buttonDarkMode.textContent = "Lightmode";
-});*/
-
-/* const cardBookmark = document.querySelector('[data-js="cardBookmark"]');
-
-cardBookmark.addEventListener("click", () => {
-  cardBookmark.classList.toggle("bookmark--clicked");
-});
-
-
 const answerButtons = document.querySelectorAll('[data-js="answerButton"]');
+const cardAnswers = document.querySelectorAll('[data-js="cardAnswer"]');
 
-answerButtons.forEach((button) => {
+answerButtons.forEach((button, answer) => {
   button.addEventListener("click", () => {
     if (button.textContent.includes("Show Answer")) {
       button.textContent = "Hide Answer, por favor";
+      cardAnswers[answer].classList.remove("hidden");
     } else {
       button.textContent = "Show Answer";
+      cardAnswers[answer].classList.add("hidden");
     }
   });
 });
