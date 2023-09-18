e62daf8466e38420186fd43cae31f9e8d5cf765e
Nils <nilsotto@gmx.de>
2023-09-18 11:33:24 +0200

    add characters left

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ const cardAnswers = document.querySelectorAll('[data-js="cardAnswer"]');
 const questionForm = document.querySelector('[data-js="questionForm"]');
 const formMain = document.querySelector('[data-js="formMain"]');
 const formQuestion = document.querySelector('[data-js="question"]');
+const char = document.querySelector('[data-js="char"]');
 
 answerButtons.forEach((button, answer) => {
   button.addEventListener("click", () => {
@@ -22,10 +23,13 @@ questionForm.addEventListener("submit", (event) => {
   const formData = new FormData(event.target);
   const data = Object.fromEntries(formData);
 
+  const questionCharactersLeft = 150 - data.question.length;
+
   const newCard = document.createElement("section");
   const newQuestion = document.createElement("p");
   const newAnswer = document.createElement("p");
   const newTags = document.createElement("p");
+  const questionCharacters = document.createElement("p");
 
   newCard.classList.add("card");
   newQuestion.textContent = data.question;
@@ -33,11 +37,15 @@ questionForm.addEventListener("submit", (event) => {
   newAnswer.classList.add("card__question");
   newAnswer.textContent = data.answer;
   newTags.classList.add("card__categories--flex");
-  newTags.textContent = data.tag;
+  newTags.textContent = "# " + data.tag;
+  questionCharacters.innerText = questionCharactersLeft;
 
+  char.append(questionCharacters);
   formMain.append(newCard);
   newCard.append(newQuestion, newAnswer, newTags);
 
   event.target.reset();
   formQuestion.focus();
+
+  console.log(formQuestion.value.length);
 });
